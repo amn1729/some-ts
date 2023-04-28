3a2bf6176c98b72525bc2bdd5e225afcb5a641ee
Aman <aman.kumar@softsensor.ai>
2023-04-28 12:29:54 +0530

    Updated Some.ts

--- Some.ts
+++ Some.ts
@@ -5,27 +5,28 @@
 // Usage: Some.String(user?.name) -> "user name" or ""
 export class Some {
   static Array<T>(arr?: Array<T> | any, defaultValue?: Array<T>): Array<T> {
-    if (arr === undefined) return [];
+    if (arr === undefined) return defaultValue || [];
     if (Array.isArray(arr)) return arr;
     return defaultValue || [];
   }
 
   static String(str?: any, defaultValue?: string): string {
-    if (str === undefined) return "";
+    if (str === undefined) return defaultValue || "";
     if (typeof str === "number") return str.toString();
     if (typeof str === "string") return str;
     return defaultValue || "";
   }
 
   static Number(num?: any, defaultValue?: number): number {
-    if (num === undefined) return 0;
+    if (num === undefined) return defaultValue || 0;
     if (typeof num === "string") return Number(num) || 0;
     if (typeof num === "number") return num;
     if (defaultValue !== undefined) return defaultValue;
     return 0;
   }
 
-  static Object(val: any, defaultValue?: object): object {
+  static Object(val?: any, defaultValue?: object): object {
+    if (val === undefined) return defaultValue || {};
     if (typeof val === "object" && !Array.isArray(val) && val !== null)
       return val;
     return defaultValue || {};
